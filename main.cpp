914515dade8c22bf44e8176eec30be44e83a4314
Santiago Scarpa <santiago.scarpa@gmail.com>
2024-11-08 15:22:01 -0300

    agrego test de tiempos

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ HANDLE mutex;        // identifica el objeto de mutua exclusion, se comporta com
 HANDLE semVacio;    // semaforo que cuenta la cantidad de espacios vacios en el buffer, se comporta como semaforo contador
 HANDLE semLleno;    // semaforo que cuenta la cantidad de espacios con items en el buffer, se comporta como semaforo contador
 
-int thread_id = 0 ; //pongo una variable global de thread ID, para poder identificar los diferentes productores/consumidores en los test de mas de un productor o consumidor.
+int thread_id = 0 ; //se pone una variable global de thread ID, para poder identificar los diferentes productores/consumidores en los test de mas de un productor o consumidor.
+int msSleepProd = 1000; //se pone una variable global para el sleep de productor, esta servira para pruebas de distintas velocidades
+int msSleepCons = 1000; //se pone una variable global para el sleep de consumidor, esta servira para pruebas de distintas velocidades
 
 // Utilizo una queue de int para el buffer, este es FIFO
 queue<int> buffer;
@@ -32,7 +34,7 @@ DWORD WINAPI Productor(LPVOID lpParam) {
     // Produce un elemento
     cout << "Productor ID-"<<pid<<" : creando elemento - " << item <<endl;
 
-    Sleep(1000); // Simulo una espera para que sea legible en ejecucion
+    Sleep(msSleepProd); // Simulo una espera para que sea legible en ejecucion
 
     // Es una funcion de windows.h que espera un objeto HANDLE el cual debe estar disponible, en este caso, el semaforo de vacia para marcar que hay espacio en el buffer
     //el INFINITE hace referencia a cuandos milisegundos debe esperar por este semaforo, se pone infinite ya que no se desea que de time out
@@ -72,7 +74,7 @@ DWORD WINAPI Consumidor(LPVOID lpParam) {
 
     // "Consumo" el elemeto que saque del buffer
     std::cout << "Consumidor ID-"<<pid<<" : consumiendo elemento - " << item << std::endl;
-    Sleep(1500); // Simulo espera para que sea legible lo que pasa en el programa
+    Sleep(msSleepCons); // Simulo espera para que sea legible lo que pasa en el programa
 
     // Hago un up(semVacio) para marcar que deje un espacio vacio en el buffer
     ReleaseSemaphore(semVacio, 1, NULL);
@@ -163,6 +165,35 @@ void testProcedure2_2(){
   CloseHandle(hiloConsumidor2);
 }
 
+//Cambio tiempo de variables de sleep para Productor
+void testProcedureTiemposP(){
+  msSleepProd = 50;
+  msSleepCons = 1000;
+  HANDLE hiloProductor = CreateThread(NULL, 0, Productor, NULL, 0, NULL);
+  HANDLE hiloConsumidor = CreateThread(NULL, 0, Consumidor, NULL, 0, NULL);
+
+  WaitForSingleObject(hiloProductor, INFINITE);
+  WaitForSingleObject(hiloConsumidor, INFINITE);
+
+  CloseHandle(hiloProductor);
+  CloseHandle(hiloConsumidor);
+}
+
+//Cambio tiempo de variables de sleep para Consumidor
+void testProcedureTiemposC(){
+  msSleepProd = 1000;
+  msSleepCons = 50;
+  HANDLE hiloProductor = CreateThread(NULL, 0, Productor, NULL, 0, NULL);
+  HANDLE hiloConsumidor = CreateThread(NULL, 0, Consumidor, NULL, 0, NULL);
+
+  WaitForSingleObject(hiloProductor, INFINITE);
+  WaitForSingleObject(hiloConsumidor, INFINITE);
+
+  CloseHandle(hiloProductor);
+  CloseHandle(hiloConsumidor);
+}
+
+
 //Cuerpo del programa, aca creo los semaforos, llamo a las funciones de consumidor y productot y las ejecuto en paralelo
 int main() {
     cout<<"INICIO APPLICACION PRODUCTOR - CONSUMIDOR CON SEMAFOROS "<<endl;
@@ -187,7 +218,10 @@ int main() {
 
   //testProcedure1_2(); // 1 Prod 2 Consu
 
-  testProcedure2_2(); // 2 Prod 2 consu
+  //testProcedure2_2(); // 2 Prod 2 consu
+
+  //testProcedureTiemposP(); //Productor mas rapido que consumidor
+  testProcedureTiemposC(); //Consumidor mas rapido que Productor
 
   CloseHandle(mutex);
   CloseHandle(semVacio);
