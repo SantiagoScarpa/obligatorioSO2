aa5b32f536285b53b8f73799926a2399958d630a
Santiago Scarpa <santiago.scarpa@gmail.com>
2024-11-09 15:58:39 -0300

    cambio comentarios

--- main.cpp
+++ main.cpp
@@ -6,31 +6,30 @@ using namespace std;
 
 #define MAXIMO_BUFFER 5 //defino el tama;o del buffer
 
-// HANDLE hace referencia a la utilizacion de objetos del kernel donde,
-HANDLE mutex;        // identifica el objeto de mutua exclusion, se comporta como semaforo binario
-HANDLE semVacio;    // semaforo que cuenta la cantidad de espacios vacios en el buffer, se comporta como semaforo contador
-HANDLE semLleno;    // semaforo que cuenta la cantidad de espacios con items en el buffer, se comporta como semaforo contador
+//HANDLE hace referencia a la utilizacion de objetos del kernel que se utilizaran como semaforos
+HANDLE mutex;
+HANDLE semVacio;
+HANDLE semLleno;
 
-int thread_id = 0 ; //se pone una variable global de thread ID, para poder identificar los diferentes productores/consumidores en los test de mas de un productor o consumidor.
-int msSleepProd = 1000; //se pone una variable global para el sleep de productor, esta servira para pruebas de distintas velocidades
-int msSleepCons = 1000; //se pone una variable global para el sleep de consumidor, esta servira para pruebas de distintas velocidades
+int thread_id = 0 ; //variable global de thread ID, para identificar diferentes productores/consumidores en test
+int msSleepProd = 1000; //variable global para el sleep de productor
+int msSleepCons = 1000; //variable global para el sleep de consumidor
 
-// Utilizo una queue de int para el buffer, este es FIFO
+// Queue de int para el buffer, la misma es FIFO
 queue<int> buffer;
 
 //Funcion productora
-//DWORD es un tipo de dato de windows que se utiliza como INT, usamos esto en vez de int, porque el manejo de procesos concurrentes lo espera. Este tiene una mejor compatibilidad y consistencia que INT en windows
+//DWORD es un tipo de dato de windows que se utiliza como INT, utilizamos esto en vez de int, porque el manejo de procesos concurrentes lo espera. Este tiene una mejor compatibilidad y consistencia que INT en windows
 //WINAPI es utilizado para procesos que van a correr en hilo de windows, tiene una serie de llamadas estandarizadas para llamadas del sistema de windows
 //LPVOID, al igual que DWORD es un tipo de dato que es necesario para la programacion en hilos de windows, este es un puntero .
 DWORD WINAPI Productor(LPVOID lpParam) {
   int item = 1;
-  int pid = thread_id; //defino el ID del productor
-  thread_id++; // incremento en 1 la variable global
+  int pid = thread_id; //ID del productor a mostrar en consola
+  thread_id++;
 
   cout << "\n"<<endl;
   //Creo elementos mientras sea posible
   while (true) {
-    // Produce un elemento
     cout << "Productor ID-"<<pid<<" : creando elemento - " << item <<endl;
 
     Sleep(msSleepProd); // Simulo una espera para que sea legible en ejecucion
@@ -40,16 +39,15 @@ DWORD WINAPI Productor(LPVOID lpParam) {
     //es basicamente hacer el down(semVacio)
     WaitForSingleObject(semVacio, INFINITE);
 
-    //hago el down(mutex), o sea valido que se puede acceder al buffer con el semaforo mutex o queda esperando infinito
+    //down(mutex) para la mutua exclusion del buffer
     WaitForSingleObject(mutex, INFINITE);
-    //le agrego un item+1 al buffer
     buffer.push(item);
     item++;
 
-    //hago el up(mutex) para liberar el semaforo
+    //up(mutex)
     ReleaseMutex(mutex);
 
-    // Hago un up(semLlano) para marcar que hay un mensaje nuevo
+    //Up(semLlano) para marcar que hay un mensaje nuevo
     //el primer argumento es el semaforo, el segundo cuanto incremento, el tercero es una variable opcional del procedimiento
     ReleaseSemaphore(semLleno, 1, NULL);
   }
@@ -59,23 +57,22 @@ DWORD WINAPI Productor(LPVOID lpParam) {
 
 // Funcion Consumidor
 DWORD WINAPI Consumidor(LPVOID lpParam) {
-
-  int pid = thread_id; //defino el ID del consumidor
-  thread_id++; // incremento en 1 la variable global
+  int pid = thread_id; //ID del consumidor
+  thread_id++;
 
   while (true) {
-    // hago un down(semLleno) para ver que haya por lo menos un mensaje en el buffer, si no espero
+    // down(semLleno) para ver que haya por lo menos un mensaje en el buffer, si no espera
     WaitForSingleObject(semLleno, INFINITE);
 
-    // hago down(mutex) para acceder solo al buffer
+    //down(mutex) para acceder solo al buffer
     WaitForSingleObject(mutex, INFINITE);
     int item = buffer.front();//obtengo item de buffer
     buffer.pop(); //borro el elemento que obtuve
-    ReleaseMutex(mutex); //hago up(mutex) para marcar que esta disponible de nuevo
+    ReleaseMutex(mutex); //up(mutex)
 
     // "Consumo" el elemeto que saque del buffer
     cout << "Consumidor ID-"<<pid<<" : consumiendo elemento - " << item << endl;
-    Sleep(msSleepCons); // Simulo espera para que sea legible lo que pasa en el programa
+    Sleep(msSleepCons); // Simulo espera con variable
 
     // Hago un up(semVacio) para marcar que deje un espacio vacio en el buffer
     ReleaseSemaphore(semVacio, 1, NULL);
@@ -206,10 +203,10 @@ int main() {
   semLleno = CreateSemaphore(NULL, 0, MAXIMO_BUFFER, NULL);
 
   //seteamos la llamada de los hilos en un procedimiento aparte para poder hacer diferentes pruebas
-  //ejecucionNormal();
+  ejecucionNormal();
 
 
-  //Ahora llamo diferente series de procedures para hacer test con diferentes variables, en caso de querer correr uno o el otro, comentar el "ejecucionNormal" y los demas Test procedure
+  //Ahora se llaman diferente procedures para hacer test con diferentes variables, en caso de querer correr uno o el otro, comentar el "ejecucionNormal" y los demas Test procedure
   //testProcedure2_1(); //2 Prod 1 Consu
 
   //testProcedure1_0(); // 1 Prod 0 Consu
